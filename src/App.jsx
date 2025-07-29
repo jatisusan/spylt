5e717eb145d2329110ddfc9fe9f16e517bf8aefa
jatisusan <susurrus112@gmail.com>
2025-07-29 13:43:47 +0545

    Implement hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import gsap from "gsap";
+import Navbar from "./components/Navbar";
+import Hero from "./sections/Hero";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <Hero />
+      <div className="h-dvh border border-black"></div>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
