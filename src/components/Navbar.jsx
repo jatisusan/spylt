6c09074122fe4fef6f83b375124d8304a6a386ff
jatisusan <susurrus112@gmail.com>
2025-07-29 11:39:41 +0545

    Implement navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+  return (
+    <nav className="fixed top-0 left-0 z-50 md:p-5 p-3">
+        <img src="/images/nav-logo.svg" alt="logo" className="w-20" />
+    </nav>
+  )
+}
+
+export default Navbar
